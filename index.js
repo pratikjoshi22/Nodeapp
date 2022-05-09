f9f1a40cfaf30b1d64dd378fa99d186b673f7a8e
Pratik <joshipratik300@gmail.com>
2022-05-09 12:03:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 5000
+app.use(express.static("public"));
+app.get('/', (req, res) => {
+//   res.send('Hello World!')
+    res.sendFile("index.html");
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
